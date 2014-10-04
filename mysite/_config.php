e149816b00cbfced8c366abe5992c7ca359a69a1
Matty <matty@mattycollins.com.au>
2014-10-05 00:50:10 +1000

    - installs SilverStripe - project setup

---
+++ mysite/_config.php
@@ -0,0 +1,20 @@
+<?php
+
+global $project;
+$project = 'mysite';
+
+global $databaseConfig;
+$databaseConfig = array(
+	"type" => 'MySQLDatabase',
+	"server" => 'localhost',
+	"username" => 'root',
+	"password" => 'root',
+	"database" => 'desi',
+	"path" => '',
+);
+
+
+require_once('conf/ConfigureFromEnv.php');
+
+// Set the site locale
+i18n::set_locale('en_US');
