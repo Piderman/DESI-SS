896e581cb5de1ae69ea9ffcedda90b8df32416fc
Matty <matty@mattycollins.com.au>
2014-10-07 22:30:01 +1100

    streamlining of vehicle type

--- desi_vab/code/Craft.php
+++ desi_vab/code/Craft.php
@@ -3,35 +3,20 @@
 
 		private static $db = array(
 			"Title" => "Varchar",
-			"Vehicle" => "Varchar",
+			"Vehicle" => "Enum(array('commandModule', 'lander', 'lifter', 'plane', 'probe', 'station'))",
 			"CrewSize" => "Int",
 			"Cost" => "Int",
 			"DeltaV" => "Int",
 			"Weight" => "Int",
 			"Stages" => "Int",
 			"PayloadCapacity" => "Int"
-
 		);
-
-		// force options for known values
-		function validVehicle() {
-			return array(
-				"commandModule" => "Command Module",
-				"lander" => "Lander",
-				"lifter" => "Lifter",
-				"plane" => "Plane",
-				"probe" => "Probe",
-				"station" => "Station"
-			);
-		}
 
 
 		//----------------------------------------------------------------------------
 
 		function getCMSFields() {
 			$fields = parent::getCMSFields();
-
-			$fields->replaceField("Vehicle", new DropdownField("Vehicle", "Craft Type", $this->validVehicle()));
 
 			return $fields;
 		}
